8055c136479e107df2d8a893668fa0012bd258f7
Sargam Poudel <sargampoudel100@gmail.com>
2025-04-12 04:10:59 +0530

    revert migration

--- db/migrate/20250408202911_create_join_table_social_proof_widgets_links.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableSocialProofWidgetsLinks < ActiveRecord::Migration[7.1]
-  def change
-    create_join_table :social_proof_widgets, :links do |t|
-      # t.index [:social_proof_widget_id, :link_id]
-      # t.index [:link_id, :social_proof_widget_id]
-    end
-  end
-end
